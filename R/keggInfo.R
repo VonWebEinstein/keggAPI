3e7cc0c1706e73508956a038e24a3c5f5776816e
VonWebEinstein <vonweb@163.com>
2017-02-09 21:44:20 +0800

    add keggInfo, keggList

---
+++ R/keggInfo.R
@@ -0,0 +1,20 @@
+#' Get KEGG Database Infomation
+#'
+#' Displays the current statistics of a given database.
+#' @return A dataframe of how many entries in each sub-database.
+#' @examples
+#' keggInfo()
+#' @import stringr
+#' @export keggInfo
+#'
+
+
+keggInfo <- function(){
+  url = "http://rest.kegg.jp/info/kegg"
+  lines = readLines(url)
+  lines = grep('\\w+[0-9 ,]+entries', lines, value = TRUE)
+  DBName = str_extract(lines, '\\w+')
+  numstr = str_extract_all(lines, '\\d')
+  Entries = as.numeric(lapply(numstr, function(s) paste(s, collapse = "")))
+  return(data.frame(DBName = DBName, Entries = Entries, row.names = DBName))
+}
---
+++ R/keggList.R
@@ -0,0 +1,27 @@
+#' Get KEGG Database or Entry List
+#'
+#' Returns a list of entry identifiers and associated definition
+#' for a given database or a given set of database entries.
+#' @export keggList
+#' @aliases keggList
+#' @usage keggList(x)
+#' @param x string. Database(s) or entry[ies], using '+' to join.
+#' @return dataframe. The table of particular contents.
+#' @examples
+#' # returns the list of reference pathways
+#' keggList('pathway')
+#'
+#' # returns the list of human pathways
+#' keggList('pathway/hsa')
+#'
+#' # returns the list of a human gene and an E.coli O157 gene
+#' keggList('hsa:10458+ece:Z5100')
+#'
+
+keggList = function(dbname){
+  url = str_c('http://rest.kegg.jp/list/', dbname)
+  lines = readLines(url)
+  dtframe = str_split(lines, '(\\t| \\| )', simplify = T)
+  dtframe = as.data.frame(dtframe)
+  return(dtframe)
+}
